443f126e332f15fa8eea202ef18d006a719c3ef8
SC00bidu <you@example.com>
2024-06-02 02:02:57 +0530

    sec updated

--- src/config.js/firebase.js
+++ src/config.js/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvMlT6PUKj0QVdzYPe9Zmsj3YLGoxZNsY",
-  authDomain: "bobby2-dec84.firebaseapp.com",
-  projectId: "bobby2-dec84",
-  storageBucket: "bobby2-dec84.appspot.com",
-  messagingSenderId: "852009013863",
-  appId: "1:852009013863:web:ec0366018cb8209229bac2",
-  measurementId: "G-LFJ9R8H3ML",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASREMENT_ID,
 };
 
 // Initialize Firebase
